67b4adbf7d5b31871e9f2581b7f803d674e91c35
Kororu-lab <chch0926@yonsei.ac.kr>
2025-04-11 21:17:32 +0900

    Initial Commit

---
+++ GameMate/GameMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameMateApp.swift
+//  GameMate
+//
+//  Created by Chiwoong Hwang on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GameMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameMateTests/GameMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameMateTests.swift
+//  GameMateTests
+//
+//  Created by Chiwoong Hwang on 4/11/25.
+//
+
+import Testing
+@testable import GameMate
+
+struct GameMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
